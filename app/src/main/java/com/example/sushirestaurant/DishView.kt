ef6a23053beb2cdef3b6aa0424c5d829dbe3a297
Noa Erben <noa.erben@post.runi.ac.il>
2024-04-06 20:47:02 +0300

    added price labels

--- app/src/main/java/com/example/sushirestaurant/DishView.kt
+++ app/src/main/java/com/example/sushirestaurant/DishView.kt
@@ -7,6 +7,7 @@ import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.annotation.DrawableRes
 import android.graphics.Color
+import androidx.core.content.ContextCompat
 
 class DishView @JvmOverloads constructor(
     context: Context,
@@ -17,12 +18,14 @@ class DishView @JvmOverloads constructor(
     private val imageView: ImageView
     private val labelText: TextView
     private val descriptionText: TextView
+    private val price : TextView
 
     init {
         inflate(context, R.layout.dish_view, this)
         imageView = findViewById(R.id.image_view)
         labelText = findViewById(R.id.label_text)
         descriptionText = findViewById(R.id.descriptionText)
+        price = findViewById(R.id.price)
         attrs?.let { setAttributes(context, it) }
     }
 
@@ -32,6 +35,8 @@ class DishView @JvmOverloads constructor(
         val labelText = typedArray.getString(R.styleable.DishView_labelText)
         val labelColor = typedArray.getColor(R.styleable.DishView_labelColor, Color.BLACK)
         val descriptionText = typedArray.getString(R.styleable.DishView_descriptionText)
+        val priceText =  typedArray.getString(R.styleable.DishView_price)
+        val priceColor = typedArray.getColor(R.styleable.DishView_priceColor, ContextCompat.getColor(context, R.color.purple))
         typedArray.recycle()
 
         if (imageRes != 0) {
@@ -46,8 +51,13 @@ class DishView @JvmOverloads constructor(
             this.descriptionText.text = descriptionText
         }
 
+        if (priceText != null) {
+            this.price.text = priceText
+        }
+
         this.labelText.setTextColor(labelColor)
         this.descriptionText.setTextColor(labelColor)
+        this.price.setTextColor(priceColor)
     }
 
     fun setImageSrc(@DrawableRes imageRes: Int) {
